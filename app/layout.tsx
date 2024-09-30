d2caca24e7a5de56b763b51977574bd6f7d0c25d
Casey DeMattina <cdemattina@ket.org>
2024-09-30 10:55:45 -0400

    initial seo work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,51 @@
-import { gql } from '@apollo/client';
-import { getClient } from '@faustwp/experimental-app-router';
-import Link from 'next/link';
-import '@/faust.config.js';
-import { FaustProvider } from '@faustwp/experimental-app-router/ssr';
+import { gql } from "@apollo/client";
+import { getClient } from "@faustwp/experimental-app-router";
+import Link from "next/link";
+import "@/faust.config.js";
+import { FaustProvider } from "@faustwp/experimental-app-router/ssr";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Arena Strength & Performance",
+    description: "Lexington Kentucky\'s premier weight training gym.",
+};
+
 export default async function RootLayout({ children }) {
-  const client = await getClient();
+    const client = await getClient();
 
-  const { data } = await client.query({
-    query: gql`
-      query GetLayout {
-        generalSettings {
-          title
-          description
-        }
-        primaryMenuItems: menuItems(where: { location: PRIMARY }) {
-          nodes {
-            id
-            label
-            uri
-          }
-        }
-        footerMenuItems: menuItems(where: { location: FOOTER }) {
-          nodes {
-            id
-            label
-            uri
-          }
-        }
-      }
-    `,
-  });
+    const { data } = await client.query({
+        query: gql`
+            query GetLayout {
+                generalSettings {
+                    title
+                    description
+                }
+                primaryMenuItems: menuItems(where: { location: PRIMARY }) {
+                    nodes {
+                        id
+                        label
+                        uri
+                    }
+                }
+                footerMenuItems: menuItems(where: { location: FOOTER }) {
+                    nodes {
+                        id
+                        label
+                        uri
+                    }
+                }
+            }
+        `,
+    });
 
-  return (
-    <html lang="en">
-      <body>
-        <FaustProvider>
-          <header>            
-          </header>
-          {children}
-        </FaustProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body>
+                <FaustProvider>
+                    <header></header>
+                    {children}
+                </FaustProvider>
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,118 @@
 import { getClient } from "@faustwp/experimental-app-router";
 import { gql } from "@apollo/client";
-import Link from "next/link";
+export let metadata = {
+    openGraph: {},
+    robots: {}
+};
+import parse from "html-react-parser";
 
 // local docker devving: https://www.youtube.com/watch?v=mi8kpAgHYFo&ab_channel=MaksimIvanov
 
 export default async function Home() {
     let client = await getClient();
+    let page = null;
     const postId = 8; // I am assuming this will always be the same id for the homepage
-    const { data } = await client.query({        
-        query: gql`
+    try {
+        const { data } = await client.query({
+            query: gql`
             query getHomepage {
-                pages(where: { id: ${postId} }) {
+                pages(where: { id: ${postId} }) {                    
                     nodes {
                         id
                         title
                         content
-                        slug
+                        seo {
+                            canonical
+                            title
+                            metaDesc
+                            focuskw
+                            metaRobotsNoindex
+                            metaRobotsNofollow
+                            opengraphAuthor
+                            opengraphDescription
+                            opengraphTitle
+                            opengraphDescription
+                            opengraphImage {
+                                altText
+                                sourceUrl
+                                srcSet
+                            }
+                            opengraphUrl
+                            opengraphSiteName
+                            opengraphPublishedTime
+                            opengraphModifiedTime
+                            twitterTitle
+                            twitterDescription
+                            twitterImage {
+                                altText
+                                sourceUrl
+                                srcSet
+                            }
+                            breadcrumbs {
+                                url
+                                text
+                            }
+                            cornerstone
+                            schema {
+                                pageType
+                                articleType
+                                raw
+                            }
+                            readingTime
+                            fullHead
+                        }
+                        author {
+                            node {
+                                seo {
+                                    metaDesc
+                                    metaRobotsNofollow
+                                    metaRobotsNoindex
+                                    title
+                                    social {
+                                        youTube
+                                        wikipedia
+                                        twitter
+                                        soundCloud
+                                        pinterest
+                                        mySpace
+                                        linkedIn
+                                        instagram
+                                        facebook
+                                    }
+                                }
+                            }
+                        }
                     }
+                    
                 }
             }
         `,
-    });
-
-    // grab the first page from the query
-    const page = data.pages.nodes[0] ?? null;
+        });
+        page = data.pages.nodes[0] ?? null;
+        console.log("seo", page.seo);
+        // add ALL of the seo fields to the metadata object
+        metadata.robots = {
+            index: page.seo.metaRobotsNoindex,
+            follow: page.seo.metaRobotsNofollow,        
+        }
+        metadata.openGraph = {
+            title: page.seo.opengraphTitle,
+            description: page.seo.opengraphDescription,
+            url: page.seo.opengraphUrl,
+            siteName: page.seo.opengraphSiteName,
+            images: [
+                {
+                    url: page.seo.opengraphImage.sourceUrl,
+                    width: 800,
+                    height: 600,
+                    alt: page.seo.opengraphImage.altText,
+                },
+            ],
+            locale: "en_US",
+        };
+    } catch (error) {
+        console.log(error);
+    }
 
     return (
         <main>
@@ -31,12 +120,14 @@ export default async function Home() {
                 <>
                     <h1>{page.title}</h1>
                     <section>
-                        <div dangerouslySetInnerHTML={{ __html: page.content }} />
+                        <div
+                            dangerouslySetInnerHTML={{ __html: page.content }}
+                        />
                     </section>
                 </>
             ) : (
                 <p>No page found</p>
-            )}            
+            )}
         </main>
     );
 }
